469439401050c6d3d5187560c71cf72c61dc0df1
adityarao2005 <adityarao2005@gmail.com>
2025-04-18 17:23:38 -0400

    creating some dependency injection rules

--- webcraft/src/main.cpp
+++ webcraft/src/main.cpp
@@ -1,7 +1,44 @@
 #include <fmt/core.h>
+#include "webcraft/app.hpp"
+#include <coroutine>
+#include <thread>
+#include <chrono>
 
-int main()
+using namespace std::chrono_literals;
+
+class Application : public webcraft::Application
 {
-    fmt::print("Hello, WebCraft!\n");
-    return 0;
-}
+public:
+    using super = webcraft::Application;
+
+    Application()
+    {
+        fmt::print("Application created.\n");
+    }
+
+    webcraft::ApplicationConfig init(int argc, char **argv) override
+    {
+        fmt::print("Initializing application...\n");
+        webcraft::ApplicationConfig config = super::init(argc, argv);
+        // Initialize the application configuration here
+
+        return config;
+    }
+
+    void run(webcraft::ApplicationContext &context) override
+    {
+        fmt::print("Running application...\n");
+
+        // Simulate some work
+        for (int i = 0; i < 5; ++i)
+        {
+            fmt::print("Working... {}/5\n", i + 1);
+            std::this_thread::sleep_for(1s);
+        }
+
+        fmt::print("Application finished.\n");
+        // You can use the context to manage application state, events, etc.
+    }
+};
+
+RUN_APP(Application)
